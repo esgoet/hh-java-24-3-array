2a2205a4e1fd65cad7b00ec4bdcbfe76c6d0c22f
esgoet <toddesgo@gmail.com>
2024-06-27 11:10:40 +0200

    add two-dim loops pract

---
+++ src/main/java/Loops.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Loops {
+    public static void main(String[] args) {
+        int[][] twoDArray = {{2,3},{4,5}};
+
+        System.out.println(Arrays.toString(twoDArray[1])); //Output: [4,5]
+        System.out.println(twoDArray[0][0]); //Output: 2
+        System.out.println(twoDArray[0][1]); //Output: 3
+    }
+}
